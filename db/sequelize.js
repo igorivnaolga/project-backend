a8bce33f60d21cd8c6ab5f4104bb8f9163064ba7
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-08-01 02:40:54 +0300

    add databaseConfig

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,15 +1,6 @@
 import { Sequelize } from 'sequelize';
+import { databaseConfig } from './config.js';
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  username: 'igorivna',
-  database: 'my_movies_uqaa',
-  password: 'dB2VDjIkaqbiQODj2pl6KXXuYi0RYuD4',
-  host: 'dpg-cqlb3h9u0jms7389keeg-a.singapore-postgres.render.com',
-  port: '5432',
-  dialectOptions: {
-    ssl: true,
-  },
-});
+const sequelize = new Sequelize(databaseConfig);
 
 export default sequelize;
